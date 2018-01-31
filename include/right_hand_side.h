f6fc1a88a425ee616cdf5728c38f94fa35251a3d
Alex Aksentyev <alexaksentyev@gmail.com>
2018-01-31 17:58:49 +0100

    constructor + moved state_type dypedef to element.h

--- include/right_hand_side.h
+++ include/right_hand_side.h
@@ -1,15 +1,26 @@
+// The Right Hand Side of the differential equation
+// corresponding to a particular element
+
+
+
 #ifndef RIGHT_HAND_SIDE_H
 #define RIGHT_HAND_SIDE_H
 
 #include <vector>
+#include "element.h"
+#include "particle.h"
+#include <boost/shared_ptr.hpp>
 
-typedef std::vector<double> state_type;
+using Element_ptr = boost::shared_ptr<Element>;
+using Particle_ptr = boost::shared_ptr<Particle>;
 
 class RightHandSide{
-  double friction_coef_;
+  Element_ptr element_;
+  Particle_ptr particle_;
 	
 public:
-  RightHandSide(double friction_coef) : friction_coef_(friction_coef){}
+  Particle_ptr particle_;
+  RightHandSide(Element_ptr host, Particle_ptr reference);
   void operator() (const state_type &x , state_type &dxdt, const double /* t*/);
 };
 
--- src/right_hand_side.cc
+++ src/right_hand_side.cc
@@ -1,10 +1,17 @@
 #include "right_hand_side.h"
 
+RightHandSide::RightHandSide(Element_ptr host,
+			     Particle_ptr reference){
+
+  element_ = host;
+  particle_ = reference;
+}
+
 
 void RightHandSide::operator() (const state_type &x,
 				state_type &dxdt,
 				const double /* t*/){
   dxdt[0] = x[1];
-  dxdt[1] = -x[0] - friction_coef_*x[1];
+  dxdt[1] = -x[0] - x[1];
 }
 
