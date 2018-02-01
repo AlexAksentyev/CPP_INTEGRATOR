193cd30b92dcfb424ff3184c603cee243782a75c
Alex Aksentyev <alexaksentyev@gmail.com>
2018-02-01 13:05:15 +0100

    test: repeating a vector, multiplying by matrix

---
+++ test/eigen_lib/eigen_test.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+
+#include <math.h>
+
+
+using namespace Eigen;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  if (argc < 3){
+    cout << "Insufficient arguments\n";
+    return 1;
+  }
+
+  double angle = atof(argv[1])*M_PI/180.;
+  double cosA = cos(angle);
+  double sinA = sin(angle);
+
+  int num_states = atoi(argv[2]);
+  
+  Matrix3d tilt_M(3, 3);
+  tilt_M <<
+    cosA, 0, -sinA,
+    0,    1, 0,
+    sinA, 0, cosA;
+
+   cout << "here\n\n";
+  
+   Vector3d field(1, 0, 0);
+   Matrix<double, 3, Dynamic> vec_field(3, num_states);
+   vec_field = field.replicate(1, num_states);
+
+  cout << "field: \n";
+  cout << vec_field  << endl;
+
+  cout << "tilt matrix: \n";
+  cout << tilt_M << endl << endl;
+
+  cout << "tilted field: \n";
+  cout << tilt_M * vec_field << endl << endl;
+}
