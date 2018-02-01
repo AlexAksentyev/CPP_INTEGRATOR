69d7f0e26eb0f35d531e96bd88419082c404b050
Alex Aksentyev <alexaksentyev@gmail.com>
2018-02-01 14:00:56 +0100

    added VAR_NUM constant

--- include/right_hand_side.h
+++ include/right_hand_side.h
@@ -1,6 +1,7 @@
 // The Right Hand Side of the differential equation
 // corresponding to a particular element
-
+// has a pointer to the element for the computation of
+// the EM fields
 
 
 #ifndef RIGHT_HAND_SIDE_H
@@ -11,6 +12,18 @@
 #include "particle.h"
 #include <boost/shared_ptr.hpp>
 
+static const int VAR_NUM = 3; // 3 for testing purposes,
+                              // otherwise 12
+// variable names
+/*   x, y, s, */
+/*   t, Theta, H, */
+/*   px, py, dK, */
+/*   Sx, Sy, Sz */ 
+
+class Element;
+using state_type = Eigen::Matrix<double, Eigen::Dynamic, VAR_NUM>;
+using state_type_ptr = boost::shared_ptr<state_type>;
+
 using Element_ptr = boost::shared_ptr<Element>;
 using Particle_ptr = boost::shared_ptr<Particle>;
 
@@ -19,7 +32,6 @@ class RightHandSide{
   Particle_ptr particle_;
 	
 public:
-  Particle_ptr particle_;
   RightHandSide(Element_ptr host, Particle_ptr reference);
   void operator() (const state_type &x , state_type &dxdt, const double /* t*/);
 };
