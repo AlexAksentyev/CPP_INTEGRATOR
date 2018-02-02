a7b480942bd007df99f08c9da06aa8a59343414f
Alex Aksentyev <alexaksentyev@gmail.com>
2018-02-02 17:13:39 +0100

    renamed variable correction

--- src/dipole.cc
+++ src/dipole.cc
@@ -4,7 +4,7 @@
 // B = (0, By, 0)
 MDipole::MDipole(double length, Particle_ptr reference_particle,
 		 double B_vert, std::string name)
-  : Element(By*CLIGHT/reference_particle->Pc()*1e-6, length, name){
+  : Element(B_vert*CLIGHT/reference_particle->Pc()*1e-6, length, name){
 
   B_field_base_(1) = B_vert;
 }
