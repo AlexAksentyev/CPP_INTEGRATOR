8b07d087bc84189a18247199f9c5590518ca3268
Alex Aksentyev <alexaksentyev@gmail.com>
2018-02-06 17:16:51 +0100

    testing variable_col class

--- test/eigen_lib/eigen_test.cc
+++ test/eigen_lib/eigen_test.cc
@@ -8,28 +8,46 @@ using namespace Eigen;
 using namespace std;
 
 using state_type = Matrix<double, Dynamic, 3>;
+template<typename T>
+using Vector = Matrix<T, Dynamic, 1>;
+template<typename T>
+using fn_ptr = T (*)(T);
 
 void modify(state_type& state){
   state.col(1) += 1*VectorXd::Ones(state.rows());
 }
 
+template<typename T>
+class variable_col {
+public:
+  Vector<T> data;
+
+  variable_col(Vector<T> given, fn_ptr<T> fn){
+    int num = given.rows();
+    data.setZero(num);    
+    for (int i=0; i<num; i++)
+      data[i] = fn(given[i]);
+  }
+  
+};
+
+template<typename T>
+T add1(T x){return x+1;}
 
 int main(int argc, char** argv)
 {
-  if (argc < 2)
-    return 1;
 
   int num_states = atoi(argv[1]);
+  int which = atoi(argv[2]);
 
   state_type state(num_states, 3);
 
   for(int i=0; i<num_states; i++)
     state.row(i) << 1, 2, 3;
 
-  cout << "state : \n" << state << endl << endl;
+  fn_ptr<double> add1_ptr = &add1;
+  variable_col<double> v(state.col(which), add1_ptr);
 
-  modify(state);
-  
-  cout << "state : \n" << state << endl << endl;
+  cout << v.data << endl;
   
 }
