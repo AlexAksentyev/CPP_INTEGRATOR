ad5742a640ee49f84ea21aa9b4f11a04d167e7b0
Alex Aksentyev <alexaksentyev@gmail.com>
2018-02-23 17:45:19 +0100

    update: generic CMakeLists for test projects

---
+++ src/test_lattice.cc
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include "right_hand_side.h"
+#include "particle.h"
+#include "drift_space.h"
+#include "dipole.h"
+#include "quadrupole.h"
+#include "sextupole.h"
+#include "wien_filter.h"
+#include "rf_element.h"
+#include <string>
+#include <stdlib.h>
+#include <vector>
+
+
+int main(int argc, char** argv) {
+  using namespace std;
+
+  cout << "boo" << endl;
+  
+  
+  return 0;
+}
