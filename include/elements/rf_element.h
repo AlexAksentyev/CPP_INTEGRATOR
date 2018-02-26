e62b38fb401af5f048e5cca13bea63c214979474
Alex Aksentyev <alexaksentyev@gmail.com>
2018-02-26 15:07:08 +0100

    RF element: new constructor

--- include/elements/rf_element.h
+++ include/elements/rf_element.h
@@ -1,6 +1,3 @@
-// TODO :
-// * Finish the RF element
-
 
 #ifndef RF_ELEMENT_H
 #define RF_ELEMENT_H
@@ -8,6 +5,15 @@
 #include "element.h"
 #include "particle.h"
 #include <math.h>
+
+
+struct RFPars { // container for RF parameters
+  double length, E_field, phase;
+  int H_number;
+
+  RFPars(double length=0, double E_field=15e5, double phase=.5*M_PI, int H_number=50)
+    : length(length), E_field(E_field), phase(phase), H_number(H_number) {}
+};
 
 
 class ERF : public Element {
@@ -20,6 +26,14 @@ public:
       double length, double acc_length,
       double E_field=15e5, double phase=0.5*M_PI, int H_number=50,
       std::string name="RF");
+
+  ERF(Particle& reference_particle,
+      double acc_length,
+      RFPars rf_pars,
+      std::string name="RF")
+    : ERF(reference_particle, rf_pars.length, acc_length,
+	  rf_pars.E_field, rf_pars.phase, rf_pars.H_number,
+	  name){}
 
   VectorizedField EField(State state);
   VectorizedField EField_prime_s(State state);
