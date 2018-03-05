25bf37fbadcce9ae686673e1f4a87bbc68c01487
Alex Aksentyev <alexaksentyev@gmail.com>
2018-03-05 16:03:08 +0100

    Python script for generating run configs (initial state, paticle)

---
+++ Lattices/config/generate_configs.py
@@ -0,0 +1,16 @@
+import os, sys
+
+home_dir = os.getenv("HOME")
+py_integrator_root = home_dir + "/REPOS/INTEGRATOR"
+sys.path.append(py_integrator_root) # add INTEGRATOR repo to pythonpath
+
+from particle_log import StateList
+from particle import Particle
+
+
+# generate initial state
+state = StateList(Sz=1, dK=(-1e-4, 1e-4, 5), x=(-1e-3, 2e-4, 4))
+state.write_to_file("state.csv", ".")
+
+deu = Particle()
+deu.write_to_file("particle.csv", ".")
