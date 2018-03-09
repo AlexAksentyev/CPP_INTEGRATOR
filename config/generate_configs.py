75bb67dae9a7bcfcc762375824505a2df955d6e4
Alex Aksentyev <alexaksentyev@gmail.com>
2018-03-09 14:03:00 +0100

    Python generator: ADAPTED to directory structure + adjustment of particle kinetic energy

--- config/generate_configs.py
+++ config/generate_configs.py
@@ -7,7 +7,7 @@ sys.path.append(py_integrator_root) # add INTEGRATOR repo to pythonpath
 from particle_log import StateList
 from particle import Particle
 
-out_dir = home_dir + "/REPOS/CPP_INTEGRATOR/Lattices/config"
+out_dir = home_dir + "/REPOS/CPP_INTEGRATOR/config"
 # generate initial state
 state = StateList(Sz=1, dK=(-1e-4, 1e-4, 5), x=(-1e-3, 2e-4, 4))
 state.write_to_file("state.conf", out_dir)
@@ -15,6 +15,7 @@ print("Generated state:")
 print(state)
 # generate particle data
 particle = Particle()
+particle.kinetic_energy += .5e-6*particle.kinetic_energy
 particle.write_to_file("particle.conf", out_dir)
 print("Generated particle:")
 print(particle)
